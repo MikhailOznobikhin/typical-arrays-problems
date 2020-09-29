1d651844043e42f71f83d853197dcc234ea650ff
Mishgen1 <32348690+Mishgen1@users.noreply.github.com>
2020-09-29 13:30:49 +0800

    init: start typecal array problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(!array || array.length === 0) return 0;
+  let a = 10000;
+  array.forEach(item => {
+    if(a > item){
+      a = item
+    }
+  });
+  return a;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(!array || array.length  === 0) return 0;
+  let a = 0;
+  array.forEach(item => {
+    if(a < item){
+      a = item
+    }
+  });
+  return a;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(!array || array.length  === 0) return 0;
+  let a = 0
+  array.forEach(item => {
+      a = a+item
+  });
+  
+  return a / array.length;
 }
